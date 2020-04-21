fab94ecc4d9d8326e03a11dc6f7d6a1220a554e2
like-programmer <dashutkadevi@gmail.com>
2020-04-21 20:50:40 +0300

    HW_2. Add webpack config. Move create functions to components & add import/export

---
+++ src/components/load-btn.js
@@ -0,0 +1,5 @@
+export const createLoadBtnTemplate = () => {
+  return (`
+    <button class="load-more" type="button">load more</button>
+    `);
+};
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`, // режим сборки
+  entry: `./src/main.js`, // точка входа приложения
+  output: { // настройка выходного файла
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`),
+  },
+  devtool: `source-map`, // подключаем sourcemaps
+  devServer: {
+    contentBase: path.join(__dirname, `public`), // где искать сборку
+    // publicPath: 'http://localhost:8080/', //веб адрес сбоки
+    // compress: true, // сжатие
+    // автоматическая перезагрузка стр
+    // если не работает по стандартному url в браузере: http://localhost:8080/, то добавить к нему http://localhost:8080/webpack-dev-server/
+    watchContentBase: true,
+  }
+};
