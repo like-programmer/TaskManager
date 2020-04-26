1b2eb88d342bd6371110e6f6d54c69bbfce846b4
like-programmer <dashutkadevi@gmail.com>
2020-04-26 17:53:23 +0300

    change function of generating filter's mock data

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -1,34 +1,12 @@
+const filterNames = [`all`, `overdue`, `today`, `favorites`, `repeating`, `tags`, `archive`];
+
 const generateFilters = () => {
-  return [
-    {
-      name: `all`,
-      count: 42,
-    },
-    {
-      name: `overdue`,
-      count: 42,
-    },
-    {
-      name: `today`,
-      count: 42,
-    },
-    {
-      name: `favorites`,
-      count: 42,
-    },
-    {
-      name: `repeating`,
-      count: 42,
-    },
-    {
-      name: `tags`,
-      count: 42,
-    },
-    {
-      name: `archive`,
-      count: 42,
-    },
-  ];
+  return filterNames.map((it) => {
+    return {
+      name: it,
+      count: Math.floor(Math.random() * 10)
+    };
+  });
 };
 
 export {generateFilters};
