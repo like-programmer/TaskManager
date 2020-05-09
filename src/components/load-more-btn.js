8f41652217aa668e554e189540861a1e83ab9947
like-programmer <dashutkadevi@gmail.com>
2020-05-09 22:14:32 +0300

    4.1.5 rewrite load more btn to class

--- src/components/load-more-btn.js
+++ src/components/load-more-btn.js
@@ -1,5 +1,28 @@
-export const createLoadMoreBtnTemplate = () => {
+import {createDOMElement} from "../utils.js";
+
+const createLoadMoreBtnTemplate = () => {
   return (`
     <button class="load-more" type="button">load more</button>
     `);
 };
+
+export default class LoadMoreBtn {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    createLoadMoreBtnTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createDOMElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
