841d5e8bfb5f2d8cd7f16e097c461d061cad087c
like-programmer <dashutkadevi@gmail.com>
2020-05-09 22:20:08 +0300

    4.1.7 rewrite board to class

--- src/components/board.js
+++ src/components/board.js
@@ -1,7 +1,30 @@
-export const createBoardTemplate = () => {
+import {createDOMElement} from "../utils.js";
+
+const createBoardTemplate = () => {
   return (`
     <section class="board container">
         <div class="board__tasks"></div>
         </section>
     `);
 };
+
+export default class Board {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createBoardTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createDOMElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
