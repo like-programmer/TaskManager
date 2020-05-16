e5ec3b62d93eccb28e773bd7c19347e21a53b2c2
like-programmer <dashutkadevi@gmail.com>
2020-05-16 14:36:19 +0300

    5.5.1 add methods for receive sorting type & empty handler for rerender cards in 'sort.js'

--- src/components/sort.js
+++ src/components/sort.js
@@ -12,4 +12,8 @@ export default class Sort extends AbstractComponent {
   getTemplate() {
     return createSortTemplate();
   }
+  
+  getSortType() {}
+  
+  setSortTypeChangeHandler(handler) {}
 }
