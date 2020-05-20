ee9671512dee7e15cba6ff8a14f5e6ad3d065222
like-programmer <dashutkadevi@gmail.com>
2020-05-20 12:42:40 +0300

    6.1.1 create task controller

---
+++ src/controllers/task.js
@@ -0,0 +1,10 @@
+import TaskComponent from "../components/task.js";
+import TaskEditComponent from "../components/task-edit.js";
+
+import {RenderPosition, render, replace} from "../utils/render.js";
+
+export default class TaskController {
+  constructor(container) {}
+
+  render(task) {}
+}
