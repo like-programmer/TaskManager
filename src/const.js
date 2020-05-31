2d05ae5b02efc0f63dddf9300da63d91dec44ec4
like-programmer <dashutkadevi@gmail.com>
2020-05-31 21:27:44 +0300

    7.1.3 create filtration controller

--- src/const.js
+++ src/const.js
@@ -1,3 +1,12 @@
 export const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
 
 export const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+
+export const FilterType = {
+  ALL: `all`,
+  ARCHIVE: `archive`,
+  FAVOURITES: `favourites`,
+  OVERDUE: `overdue`,
+  REPEATING: `repeating`,
+  TODAY: `today`,
+};
