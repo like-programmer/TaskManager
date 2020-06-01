901dc6e9f341c067bed8b4c330d32c92dec4f69c
like-programmer <dashutkadevi@gmail.com>
2020-06-01 17:20:10 +0300

    7.1.5 set active filter type in model

--- src/controllers/filter.js
+++ src/controllers/filter.js
@@ -20,7 +20,7 @@ export default class FilterController {
 
   render() {
     const container = this._container;
-    const allTasks = this._tasksModel.getTasks();
+    const allTasks = this._tasksModel.getTasksAll();
     const filters = Object.values(FilterType).map((filterType) => {
       return {
         name: filterType,
@@ -42,7 +42,7 @@ export default class FilterController {
   }
 
   _filterChangeHandler(filterType) {
-    // this._tasksModel.setFilter(filterType);
+    this._tasksModel.setFilter(filterType);
     this._activeFilterType = filterType;
   }
 
