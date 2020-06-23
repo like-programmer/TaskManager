5a5a016245187f07639086c6d67c6668ef56c2c6
like-programmer <dashutkadevi@gmail.com>
2020-06-23 15:56:04 +0300

    8.3.1 add createTask method to the api.js

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -139,18 +139,21 @@ export default class BoardController {
         taskController.destroy();
         this._updateTasks(this._showingTaskCount);
       } else {
-        this._tasksModel.addTask(newData);
-        taskController.render(newData, TaskControllerMode.DEFAULT);
+        this._api.createTask(newData)
+          .then((taskModel) => {
+            this._tasksModel.addTask(taskModel);
+            taskController.render(taskModel, TaskControllerMode.DEFAULT);
 
-        if (this._showingTaskCount % SHOWING_TASK_COUNT_BY_BUTTON === 0) {
-          const destroyedTask = this._showedTaskControllers.pop();
-          destroyedTask.destroy();
-        }
+            if (this._showingTaskCount % SHOWING_TASK_COUNT_BY_BUTTON === 0) {
+              const destroyedTask = this._showedTaskControllers.pop();
+              destroyedTask.destroy();
+            }
 
-        this._showedTaskControllers = [].concat(taskController, this._showedTaskControllers);
-        this._showingTaskCount = this._showedTaskControllers.length;
+            this._showedTaskControllers = [].concat(taskController, this._showedTaskControllers);
+            this._showingTaskCount = this._showedTaskControllers.length;
 
-        this._renderLoadMoreBtn();
+            this._renderLoadMoreBtn();
+          });
       }
     } else if (newData === null) {
       this._tasksModel.removeTask(oldData.id);
