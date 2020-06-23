c2b079e4424364aeb584b98898a42ffcd78bab82
like-programmer <dashutkadevi@gmail.com>
2020-06-23 14:19:40 +0300

    8.2.4 API refactoring. Move same request creation logic into the private method

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import FilterController from "./controllers/filter.js";
 import {RenderPosition, render} from "./utils/render.js";
 
 const AUTHORIZATION = `Basic dXNlckBwYXNzd29yZAo=`;
+const END_POINT = `https://11.ecmascript.pages.academy/task-manager`;
 
 const dateTo = new Date();
 const dateFrom = (() => {
@@ -17,7 +18,7 @@ const dateFrom = (() => {
   return d;
 });
 
-const api = new API(AUTHORIZATION);
+const api = new API(END_POINT, AUTHORIZATION);
 const tasksModel = new TasksModel();
 
 const siteMainElement = document.querySelector(`.main`);
