a0773f6049ac52319fbf478f1ed3c1aecdad6b8c
like-programmer <dashutkadevi@gmail.com>
2020-06-23 13:28:56 +0300

    8.2.2 throw an exceprion if response status different from 200

--- src/api.js
+++ src/api.js
@@ -1,5 +1,13 @@
 import Task from "./models/task.js";
 
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
 const API = class {
   constructor(authorization) {
     this._authorization = authorization;
@@ -9,12 +17,13 @@ const API = class {
     const headers = new Headers();
     headers.append(`Authorization`, this._authorization);
     return fetch(`https://11.ecmascript.pages.academy/task-manager/tasks`, {headers})
+      .then(checkStatus)
       .then((response) => response.json())
       .then(Task.parseTasks);
   }
 
   updateTask(id, data) {
-    const headers  new Headers();
+    const headers = new Headers();
     headers.append(`Authorization`, this._authorization);
 
     return fetch(`https://11.ecmascript.pages.academy/task-manager/tasks${id}`, {
@@ -22,6 +31,7 @@ const API = class {
       body: JSON.stringify(data),
       headers,
     })
+      .then(checkStatus)
       .then((response) => response.json())
       .then(Task.parseTasks);
   }
