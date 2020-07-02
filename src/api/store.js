dcbd89e955d807f64e86cb4c45b67ceca6da8fd6
like-programmer <dashutkadevi@gmail.com>
2020-07-02 20:29:35 +0300

    9.1.5 add getItems & setItems logic into store methods

--- src/api/store.js
+++ src/api/store.js
@@ -1,9 +1,31 @@
 export default class Store {
-  getItems() {
-    return {};
+  constructor(key, storage) {
+    this._storeKey = key;
+    this._storage = storage;
   }
 
-  setItem(key, value) {}
+  getItems() {
+    try {
+      return JSON.parse(this._storage.getItem(this._storeKey)) || {};
+    } catch (error) {
+      return {};
+    }
+  }
 
-  removeItem(key) {}
+  setItem(key, value) {
+    const store = this.getItems();
+
+    this._storage.setItem(
+      this._storeKey,
+      JSON.stringify(
+        Object.assign({}, store, {
+          [key]: value,
+        }),
+      )
+    );
+
+  }
+
+  removeItem(key) {
+  }
 }
