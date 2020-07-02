598b966886d78a3d291f3d247ee4e4661a4c8a0a
like-programmer <dashutkadevi@gmail.com>
2020-07-02 20:44:04 +0300

    9.1.7 add logic into store.removeItem & provider.deleteTask methods

--- src/api/provider.js
+++ src/api/provider.js
@@ -52,10 +52,12 @@ export default class Provider {
 
   deleteTask(id) {
     if (isOnLine()) {
-      return this._api.deleteTask(id);
+      return this._api.deleteTask(id)
+        .then(() => this._store.removeItem(id));
     }
 
-    // TODO: Реализовать логику при отсутствии интернета
-    return Promise.reject(`offline logic is not implemented`);
+    this._store.removeItem(id);
+
+    return Promise.resolve();
   }
 }
